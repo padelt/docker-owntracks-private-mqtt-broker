b8189e67c897728baec44009cc31ec5060bf41c6
Philipp Adelt <autosort-github@philipp.adelt.net>
2015-06-19 23:58:13 +0200

    First public version

--- files/start.sh
+++ files/start.sh
@@ -16,12 +16,14 @@ mkdir -p $VOLUME/config/conf.d
 # Provide an initial Mosquitto config if nothing is there yet.
 if [ ! -f $VOLUME/config/mosquitto.conf ]
 then
+   echo "Setting default configuration mosquitto.conf. Feel free to improve!"
    cp /tmp/mosquitto.conf.default $VOLUME/config/mosquitto.conf
 fi
 
 # Create a minimal CA-infrastructure with a server and 10 client certs if not there yet
 if [ ! -d $VOLUME/config/tls ]
 then
+   echo "Generating fresh TLS/SSL infrastructure. Don't forget to install ca.crt on your devices!"
    mkdir -p $VOLUME/config/tls
    cd $VOLUME/config/tls
    /tmp/generate-CA.sh
@@ -37,13 +39,66 @@ fi
 # Generate some client authentication tokens and configuration files.
 if [ ! -d $CLIENTS_DIR ]
 then
+   echo "Generating users and client configuration files (.otrc). Open one of those with the app."
+   if [ -f $VOLUME/host.config ]
+   then
+      . $VOLUME/host.config
+   fi
+
+   if [ "x$HOSTNAME" == "x" ] || [ "x$PORT" == "x" ] 
+   then
+      echo "************ PLEASE FIX host.config, then remove the config/clients/ directory and run again! ************"
+      HOSTNAME="THIS.WILL.NOT.WORK.PLEASE.FIX.HOST.CONFIG.FILE.example.org"
+      PORT="1883"
+   fi
+
    mkdir -p $CLIENTS_DIR
    touch $PASSWD_FILE
    cd $CLIENTS_DIR
+   
    for ((i=1;i<=10;i++)); do
       PASSWORD=`pwgen --no-capitalize --numerals --ambiguous 14 1`
-      echo "Username client$i with Password $PASSWORD" >> $PASSWD_FILE.cleartext
-      mosquitto_passwd -b $PASSWD_FILE client$i $PASSWORD
+      USERNAME=client$i
+      echo "Username $USERNAME with Password $PASSWORD" >> $PASSWD_FILE.cleartext
+      mosquitto_passwd -b $PASSWD_FILE $USERNAME $PASSWORD
+      cat > $USERNAME.otrc <<EOF
+{
+  "ranging" : false,
+  "positions" : 50,
+  "monitoring" : 1,
+  "willTopic" : "",
+  "deviceId" : "$USERNAME",
+  "host" : "$HOSTNAME",
+  "tid" : "$i",
+  "_type" : "configuration",
+  "keepalive" : 60,
+  "pubTopicBase" : "",
+  "cmd" : false,
+  "allowRemoteLocation" : true,
+  "subTopic" : "",
+  "pubRetain" : true,
+  "willRetain" : false,
+  "updateAddressBook" : false,
+  "waypoints" : [
+
+  ],
+  "port" : $PORT,
+  "pubQos" : 1,
+  "locatorInterval" : 300,
+  "tls" : true,
+  "auth" : true,
+  "cleanSession" : true,
+  "extendedData" : true,
+  "clientId" : "$USERNAME",
+  "willQos" : 1,
+  "password" : "$PASSWORD",
+  "locatorDisplacement" : 2000,
+  "mode" : 0,
+  "subQos" : 1,
+  "username" : "$USERNAME"
+}
+EOF
+      
    done
 fi
 
